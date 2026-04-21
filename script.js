d6d60cd6f9dac113b440ff26ac99b424dc4b2b0d
dtorres3795 <dtorres3795@gmail.com>
2026-04-20 21:59:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Character Array
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
 "U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
-"t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
-"$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+"t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","!","@","#",
+"$","%","^","&","*","_","-","+","=",",","|",":",";",".","?"
+];
 
 // DOM Elements
 let password_1_El=document.getElementById("password_1_el")
@@ -13,7 +13,7 @@ let password_4_El=document.getElementById("password_4_el")
 let password_5_El=document.getElementById("password_5_el")
 let password_6_El=document.getElementById("password_6_el")
 
-// Easter Egg Tracking
+// Easter Egg 
 let click_times=[]
 
 function check_spam(){
